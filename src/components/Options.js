954032f8ae78aeed30d2540afec858c23ee578b7
pmarinheiro <p.marinheiro.silva@outlook.pt>
2024-01-21 01:29:49 +0000

    first commit

---
+++ src/components/Options.js
@@ -0,0 +1,29 @@
+import { useQuiz } from "../contexts/QuizContext";
+
+export default function Options() {
+  const { questions, dispatch, answer, index } = useQuiz();
+  const question = questions.at(index);
+
+  const hasAnswered = answer !== null;
+
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          disabled={hasAnswered}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
